400744ec18f1c8b9f89003c307513a446cdf9007
tanmaymodi <tanmaymodi24@gmail.com>
2021-09-26 12:40:29 +0530

    file upload

--- doc_classify/dc/views.py
+++ doc_classify/dc/views.py
@@ -1,15 +1,22 @@
 from django.shortcuts import render, HttpResponse
 from django.core.files.storage import FileSystemStorage
+import datetime
 # Create your views here.
 def index(request):
     
+
+    #uploading files code
     if request.method  == 'POST':
       for x, uploadedFile in enumerate(request.FILES.getlist("document")):
         print(uploadedFile.name)
         print(uploadedFile.size)
         fs = FileSystemStorage()
         fs.save(uploadedFile.name, uploadedFile)
-    return render(request, 'index.html')
+
+    daysOfWeek = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+    today = datetime.datetime.now().date()
+    context = {'allow':True,'Age':32,'Name':"","today":today,"dow":daysOfWeek}    
+    return render(request, 'index.html', context)
     # return HttpResponse("this is homepage")
 
 def about(request):
